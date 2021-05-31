410d3de93c2d2f1326f0dcd6e2cd7d2336e79502
Vojtěch Srdečný <vojtech.srdecny@gmail.com>
2021-05-31 11:15:45 +0200

    working graph

--- src/routes/select/[...dir].svelte
+++ src/routes/select/[...dir].svelte
@@ -1,21 +1,25 @@
 
-<main>
-    <h2>Directory {$page.params.dir}</h2>
-    <ul>
-        {#each dirs as dir}
-        <li>
-            <a href={`/select/${dir}`}>
-                {dir}
-            </a>
-        </li>
-        {/each}
-        <li>
-            <a href={`/select/${previousDir}`}>
-                ../
-            </a>
-        </li>
-    </ul>
-    Pipeline {isPipeline ? "" : "not "} detected!
+<main class="container">
+    <div class="dirs">
+
+        <h2>Directory {$page.params.dir}</h2>
+        <ul>
+            {#each dirs as dir}
+            <li>
+                <a href={`/select/${dir}`}>
+                    {dir}
+                </a>
+            </li>
+            {/each}
+            <li>
+                <a href={`/select/${previousDir}`}>
+                    ../
+                </a>
+            </li>
+        </ul>
+        Pipeline {isPipeline ? "" : "not "} detected!
+    </div>
+    <div class="pipeline" bind:this={refElement}></div>
 </main>
 
 <script lang="ts" context="module">
@@ -33,11 +37,53 @@
 
 <script lang="ts">
     import { page } from "$app/stores"
+    import cytoscape from "cytoscape"
+    import dagre from 'cytoscape-dagre'
+    import { onMount } from "svelte";
+
+    let refElement = null
+
+    onMount(() => {
+
+        cytoscape.use(dagre)
+        var cy = cytoscape({
+            container: refElement,
+            elements: [ // list of graph elements to start with
+                { // node a
+                data: { id: 'a' }
+                },
+                { // node b
+                data: { id: 'b' }
+                },
+                { // edge ab
+                data: { id: 'ab', source: 'a', target: 'b' }
+                }
+            ],
+        })
+
+    })
+
     export let dirs;
     export let files;
-    console.log(files)
 
     $: previousDir = $page.params.dir?.split("/").slice(0, -1).join("/") || "/"
     $: isPipeline = ["pid", "log", "err"].every(suffix => files.map(f => f.split(".").pop()).some(s => s == suffix))
 
 </script>
+
+<style>
+
+    .container {
+        display: grid;
+        grid-template-columns: 33% 66%;
+    }
+
+    .dirs {
+        grid-column: 1 / 2;
+    }
+
+    .pipeline {
+        grid-column: 2 / 2;
+        background-color: white;
+    }
+</style>
